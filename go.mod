4ba9c819e3716eb7570db5f5b8b7a2977c996efc
leocavalcante <lc@leocavalcante.com>
2023-03-06 21:23:25 -0300

    Packages

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/leocavalcante/go-intensivo
 
 go 1.13
+
+require github.com/google/uuid v1.3.0
--- main.go
+++ main.go
@@ -1,36 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type Order struct {
-	ID       string
-	Price    float64
-	Quantity int
-}
-
-func (o *Order) SetPrice(price float64) {
-	o.Price = price
-	fmt.Println("Price dentro do setPrice:", o.Price)
-}
-
-func (o Order) GetTotal() float64 {
-	return o.Price * float64(o.Quantity)
-}
-
-func NewOrder() *Order {
-	return &Order{}
-}
+	"github.com/google/uuid"
+)
 
 func main() {
-	order := NewOrder()
-	order.ID = "123"
-	order.Quantity = 10
-	order.Price = 10.0
-
-	order2 := order
-
-	order.Price = 5.0
-
-	fmt.Println(order2.Price)
-
+	fmt.Println(uuid.New().String())
 }
